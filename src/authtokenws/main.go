af4aded7fdf54449dc2b97937e233c9e08356728
UVADave <dpg3k@virginia.edu>
2016-02-23 08:23:15 -0500

    Change default database name

--- src/authtokenws/main.go
+++ src/authtokenws/main.go
@@ -14,7 +14,7 @@ func main( ) {
 	// process command line flags and serup configuration
 	flag.StringVar( &config.ServicePort, "port", "8080", "The service listen port" )
 	flag.StringVar( &config.DbHost, "dbhost", "mysqldev.lib.virginia.edu", "The database server hostname:port" )
-    flag.StringVar( &config.DbName, "dbname", "authtoken", "The database name" )
+    flag.StringVar( &config.DbName, "dbname", "authtoken_development", "The database name" )
 	flag.StringVar( &config.DbUser, "dbuser", "dbuser", "The database username" )
 	flag.StringVar( &config.DbPassphrase, "dbpassword", "dbpassword", "The database passphrase")
 
