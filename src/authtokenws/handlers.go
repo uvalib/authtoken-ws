a2fefad7bffee55b9545e52aa3ed17e586a3a3e0
UVADave <dpg3k@virginia.edu>
2016-02-25 14:06:30 -0500

    Simplify the handlers

--- src/authtokenws/handlers.go
+++ src/authtokenws/handlers.go
@@ -13,35 +13,35 @@ func TokenLookup( w http.ResponseWriter, r *http.Request ) {
     what := vars[ "what" ]
     token := vars[ "token" ]
 
-    w.Header().Set( "Content-Type", "application/json; charset=UTF-8" )
-
-    // is this a good token and
+    // is this a good token ?
     if ActivityIsOk( whom, what, token ) {
-        status := http.StatusOK
-        w.WriteHeader( status )
-        if err := json.NewEncoder( w ).Encode( Response{ Status: status, Message: http.StatusText( status ) } ); err != nil {
-            log.Fatal( err )
-        }
-        return
+        encodeResponse( w, http.StatusOK )
+    } else {
+        encodeResponse( w, http.StatusForbidden )
     }
+}
 
-    // If this token is not OK then 403
-    status := http.StatusForbidden
+func HealthCheck( w http.ResponseWriter, r *http.Request ) {
+    encodeHealthCheckResponse( w, http.StatusOK, "" )
+}
+
+func encodeResponse( w http.ResponseWriter, status int ) {
+    jsonResponse( w )
     w.WriteHeader( status )
     if err := json.NewEncoder(w).Encode( Response{ Status: status, Message: http.StatusText( status ) } ); err != nil {
         log.Fatal( err )
     }
 }
 
-func HealthCheck( w http.ResponseWriter, r *http.Request ) {
-
-    healthy := true
-    message := ""
-
-    w.Header().Set( "Content-Type", "application/json; charset=UTF-8" )
-    w.WriteHeader( http.StatusOK )
-
+func encodeHealthCheckResponse( w http.ResponseWriter, status int, message string ) {
+    healthy := status == http.StatusOK
+    jsonResponse( w )
+    w.WriteHeader( status )
     if err := json.NewEncoder(w).Encode( HealthCheckResponse { CheckType: HealthCheckResult{ Healthy: healthy, Message: message } } ); err != nil {
         log.Fatal( err )
     }
+}
+
+func jsonResponse( w http.ResponseWriter ) {
+    w.Header( ).Set( "Content-Type", "application/json; charset=UTF-8" )
 }
