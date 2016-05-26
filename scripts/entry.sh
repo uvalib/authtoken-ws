7b56ae03e01205a1187d8dde121f878624c544e7
UVADave <dpg3k@virginia.edu>
2016-05-26 12:14:59 -0400

    Add environment driven startup options

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,31 @@
-if [ -z "$DBPASSWD" ]; then
-   echo "ERROR: DBPASSWD must be defined"
-   exit 1
+# set blank options variables
+DBHOST_OPT=""
+DBNAME_OPT=""
+DBUSER_OPT=""
+DBPASSWD_OPT=""
+
+# database host
+if [ -n "$DBHOST" ]; then
+   DBHOST_OPT="--dbhost $DBHOST"
 fi
 
-bin/authtoken-ws --dbpassword $DBPASSWD
+# database name
+if [ -n "$DBNAME" ]; then
+   DBNAME_OPT="--dbname $DBNAME"
+fi
+
+# database user
+if [ -n "$DBUSER" ]; then
+   DBUSER_OPT="--dbuser $DBUSER"
+fi
+
+# database password
+if [ -n "$DBPASSWD" ]; then
+   DBPASSWD_OPT="--dbpassword $DBPASSWD"
+fi
+
+bin/authtoken-ws $DBHOST_OPT $DBNAME_OPT $DBUSER_OPT $DBPASSWD_OPT
+
+#
+# end of file
+#
