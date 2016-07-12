43abe8295d746a5a6599a527229fbc27386823e7
UVADave <dpg3k@virginia.edu>
2016-07-12 09:59:46 -0400

    LIBRA-381: correct location for version tag

--- src/authtokenws/version.go
+++ src/authtokenws/version.go
@@ -6,9 +6,9 @@ import (
 )
 
 func Version( ) string {
-    files, _ := filepath.Glob( "/buildtag.*" )
+    files, _ := filepath.Glob( "buildtag.*" )
     if len( files ) == 1 {
-       return strings.Replace( files[ 0 ], "/buildtag.", "", 1 )
+       return strings.Replace( files[ 0 ], "buildtag.", "", 1 )
     }
 
     return "unknown"
