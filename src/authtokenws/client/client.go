0fa770e9fde5ee6b02548afc2f49ec75f67eae45
UVADave <dpg3k@virginia.edu>
2017-01-06 13:52:08 -0500

    Add test case for runtime information

--- src/authtokenws/client/client.go
+++ src/authtokenws/client/client.go
@@ -7,6 +7,8 @@ import (
     "net/http"
     "encoding/json"
     "authtokenws/api"
+    "io"
+    "io/ioutil"
 )
 
 func HealthCheck( endpoint string ) int {
@@ -53,6 +55,33 @@ func VersionCheck( endpoint string ) ( int, string ) {
     }
 
     return resp.StatusCode, r.Version
+}
+
+func RuntimeCheck( endpoint string ) ( int, * api.RuntimeResponse ) {
+
+    url := fmt.Sprintf( "%s/runtime", endpoint )
+    //fmt.Printf( "%s\n", url )
+
+    resp, body, errs := gorequest.New( ).
+            SetDebug( false ).
+            Get( url  ).
+            Timeout( time.Duration( 5 ) * time.Second ).
+            End( )
+
+    if errs != nil {
+        return http.StatusInternalServerError, nil
+    }
+
+    defer io.Copy( ioutil.Discard, resp.Body )
+    defer resp.Body.Close( )
+
+    r := api.RuntimeResponse{ }
+    err := json.Unmarshal( []byte( body ), &r )
+    if err != nil {
+        return http.StatusInternalServerError, nil
+    }
+
+    return resp.StatusCode, &r
 }
 
 func Auth( endpoint string, whom string, what string, token string ) int {
